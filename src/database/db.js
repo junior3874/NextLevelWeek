e53ca1c7b6619dde155b83825896fbf6a7e7b681
Wellington Rufino <wellingtonjr53@outlook.com.br>
2020-09-21 22:40:19 -0300

    removed desnecessary archives

--- src/database/db.js
+++
@@ -1,8 +0,0 @@
-//importar a sua dependência
-
-const sqlite3 = require("sqlite3").verbose();
-
-// criar o objeto de banco de dados
-const db = new sqlite3.Database("./src/database/database.db");
-
-module.exports = db;
